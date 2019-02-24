42b12d559ad680da5397e55e7249f739b0741cbb
Jon Yu <jon.yu.1105@gmail.com>
2019-02-24 15:40:09 -0500

    Click to move with layer filtering.

--- Lumos RPG/Assets/Scripts/Cursor.cs
+++ Lumos RPG/Assets/Scripts/Cursor.cs
@@ -14,6 +14,6 @@ public class Cursor : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         
-        Debug.Log(cameraRaycaster.layerHit);
+        //Debug.Log(cameraRaycaster.layerHit);
 	}
 }
---
+++ Lumos RPG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof (ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float walkMoveStopRadius = 0.2f;
+
+    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+        
+    private void Start()
+    {
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            print(cameraRaycaster.layerHit);
+            print("Cursor raycast hit " + cameraRaycaster.hit.collider.gameObject.name.ToString());
+
+            switch (cameraRaycaster.layerHit) {
+                case Layer.Walkable:
+                    currentClickTarget = cameraRaycaster.hit.point;
+                    //if Move is here, will only move when button is held down.
+                    break;
+                case Layer.Enemy:
+                    print("Not moving towards enemy");
+                    break;
+                default:
+                    print("Unexpected case");
+                    return;
+            }
+
+            //currentClickTarget = cameraRaycaster.hit.point;  // So not set in default case
+        }
+        //Using the move outside the input If will allow you to move even after releasing the button
+        var playerToClickPoint = transform.position - currentClickTarget;
+        if (playerToClickPoint.magnitude >= walkMoveStopRadius) {
+            m_Character.Move(currentClickTarget - transform.position, false, false);
+        }
+        else {
+            m_Character.Move(Vector3.zero, false, false);
+        }
+        
+
+
+        //if (cameraRaycaster.layerHit == Layer.Walkable) {
+        //    m_Character.Move(currentClickTarget - transform.position, false, false);
+        //}
+
+    }
+}
+
