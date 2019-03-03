f88b0085d25477cde98391c8a5b0e3ae40f81423
Jon Yu <jon.yu.1105@gmail.com>
2019-03-03 15:16:42 -0500

    Player movement using mouse, keyboard, or gamepad.

--- Lumos RPG/Assets/Player/PlayerMovement.cs
+++ Lumos RPG/Assets/Player/PlayerMovement.cs
@@ -8,9 +8,11 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] float walkMoveStopRadius = 0.2f;
 
-    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
-    CameraRaycaster cameraRaycaster;
-    Vector3 currentClickTarget;
+    private ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    private CameraRaycaster cameraRaycaster;
+    private Vector3 currentClickTarget;
+
+    private bool isMouseMode = true;
         
     private void Start()
     {
@@ -19,14 +21,50 @@ public class PlayerMovement : MonoBehaviour
         currentClickTarget = transform.position;
     }
 
-    // TODO fix issue with click to move and WASD movement stacking movement speed.
-
-
     // Fixed update is called in sync with physics
-    private void FixedUpdate()
-    {
-        if (Input.GetMouseButton(0))
-        {
+    private void FixedUpdate() {
+        // TODO add to keybinding menu
+        if (Input.GetKeyDown(KeyCode.G)) { //G for gamepad
+            isMouseMode = !isMouseMode;
+        }
+
+        if (isMouseMode) {
+            ProcessMouseMovement();
+        }
+        else {
+            ProcessDirectMovement();
+        }
+
+
+
+
+        //if (cameraRaycaster.layerHit == Layer.Walkable) {
+        //    m_Character.Move(currentClickTarget - transform.position, false, false);
+        //}
+
+    }
+
+    private void ProcessDirectMovement() {
+        // read inputs
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        //bool crouch = Input.GetKey(KeyCode.C);
+
+        // calculate camera relative direction to move:
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+#if !MOBILE_INPUT
+        // walk speed multiplier
+        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+#endif
+
+        // pass all parameters to the character control script
+        m_Character.Move(m_Move, false, false);
+    }
+
+    private void ProcessMouseMovement() {
+        if (Input.GetMouseButton(0)) {
             print(cameraRaycaster.layerHit);
             print("Cursor raycast hit " + cameraRaycaster.hit.collider.gameObject.name.ToString());
 
@@ -53,13 +91,6 @@ public class PlayerMovement : MonoBehaviour
         else {
             m_Character.Move(Vector3.zero, false, false);
         }
-        
-
-
-        //if (cameraRaycaster.layerHit == Layer.Walkable) {
-        //    m_Character.Move(currentClickTarget - transform.position, false, false);
-        //}
-
     }
 }
 
