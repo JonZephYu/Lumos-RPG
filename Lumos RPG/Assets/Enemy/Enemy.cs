e7898414e7c8a0ebe6b4aac2d35641ce1757d24e
Jon Yu <jon.yu.1105@gmail.com>
2019-03-17 17:55:42 -0400

    Enemy health bars attached to Enemy prefab using a worldspace canvas + UI Socket GO on the enemy.  Health bar also faces the camera in LateUpdate.

---
+++ Lumos RPG/Assets/Enemy/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float maxHealthPoints = 1000;
+    private float currentHealthPoints = 1000;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float healthAsPercentage {
+        get { return currentHealthPoints / (float)maxHealthPoints; }
+    }
+
+}
