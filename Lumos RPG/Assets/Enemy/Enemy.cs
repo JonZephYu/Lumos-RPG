e875a4228add2e01b096d149ab17a46b94f8be67
Jon Yu <jon.yu.1105@gmail.com>
2019-04-03 20:07:44 -0400

    AI path finding, terrain nav mesh, aggro and leash radius.

--- Lumos RPG/Assets/Enemy/Enemy.cs
+++ Lumos RPG/Assets/Enemy/Enemy.cs
@@ -1,21 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour {
 
     [SerializeField] float maxHealthPoints = 1000;
+    [SerializeField] float aggroRadius = 1f;
+    [SerializeField] float leashRadius = 1f;
     private float currentHealthPoints = 1000;
+
+    GameObject player = null;
+    AICharacterControl aiCharacterControl = null;
     
 
 	// Use this for initialization
 	void Start () {
-		
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacterControl = GetComponent<AICharacterControl>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+
+        if (distanceToPlayer <= aggroRadius) {
+            aiCharacterControl.SetTarget(player.transform);
+        }
+        else {
+            aiCharacterControl.SetTarget(transform);
+        }
 	}
 
     public float healthAsPercentage {
