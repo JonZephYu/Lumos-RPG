087dd82a662cba542deaf597abafb0e2b2de3b2c
Jon Yu <jon.yu.1105@gmail.com>
2019-04-20 19:25:15 -0400

    Added attackRadius SF and gizmos to represent aggro, leash, and attack range.

--- Lumos RPG/Assets/Enemy/Enemy.cs
+++ Lumos RPG/Assets/Enemy/Enemy.cs
@@ -7,30 +7,34 @@ public class Enemy : MonoBehaviour, IDamageable {
 
     [SerializeField] float maxHealthPoints = 1000;
     [SerializeField] float aggroRadius = 1f;
-    [SerializeField] float leashRadius = 1f;
+    [SerializeField] float leashRadius = 3f;
+    [SerializeField] float attackRadius = 2f;
     private float currentHealthPoints = 1000;
 
     GameObject player = null;
     AICharacterControl aiCharacterControl = null;
-    
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
         aiCharacterControl = GetComponent<AICharacterControl>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+    }
 
-        if (distanceToPlayer <= aggroRadius) {
+    // Update is called once per frame
+    void Update() {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (distanceToPlayer <= attackRadius){
+            //TODO Spawn projectile
+            Debug.Log(gameObject.name + " is attacking!");
+        }
+        else if (distanceToPlayer <= aggroRadius) {
             aiCharacterControl.SetTarget(player.transform);
         }
         else {
             aiCharacterControl.SetTarget(transform);
         }
-	}
+    }
 
     public float healthAsPercentage {
         get { return currentHealthPoints / (float)maxHealthPoints; }
@@ -38,6 +42,17 @@ public class Enemy : MonoBehaviour, IDamageable {
 
     public void TakeDamage(float damage) {
         currentHealthPoints = Mathf.Clamp(currentHealthPoints - damage, 0f, maxHealthPoints);
+    }
+
+
+    private void OnDrawGizmos() {
+        //Draw movement gizmos
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, aggroRadius);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, leashRadius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
     }
 
 }
