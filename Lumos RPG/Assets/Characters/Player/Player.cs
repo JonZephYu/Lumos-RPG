5efae5cadd7303bf7ea656dcbdb0ef4605227dfd
Jon Yu <jon.yu.1105@gmail.com>
2019-05-18 17:28:33 -0400

    Roughly have player projectiles shooting towards mouse

--- Lumos RPG/Assets/Characters/Player/Player.cs
+++ Lumos RPG/Assets/Characters/Player/Player.cs
@@ -10,13 +10,22 @@ public class Player : MonoBehaviour, IDamageable {
     [SerializeField] float swingTimer = 1f;
     [SerializeField] float attackRange = 5f;
 
+    //Projectile info
+    [SerializeField] float damagePerShot = 10f;
+    [SerializeField] float attackTimer = 1f;
+    [SerializeField] float projectileLifetime = 1f;
+    [SerializeField] float projectileSpeed = 1f;
+    [SerializeField] Vector3 aimOffset = new Vector3(0f, 1f, 0f);
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] GameObject projectileSocket;
+
 
     //TODO solve serialize and const confliction
     [SerializeField] const int walkableLayer = 8;
     [SerializeField] const int enemyLayer = 9;
     [SerializeField] const int stiffLayer = 10;
 
-
+    private bool isAttacking = false;
     private float currentHealthPoints;
     private GameObject currentTarget;
     private CameraRaycaster cameraRaycaster;
@@ -40,6 +49,15 @@ public class Player : MonoBehaviour, IDamageable {
     }
 
     private void OnMouseClick(RaycastHit raycastHit, int layerHit) {
+        if (!isAttacking) {
+            isAttacking = true;
+            // TODO switch to coroutine, consider attack speed instead of attack delay
+            InvokeRepeating("Attack", 0f, attackTimer);
+        }
+        else {
+            CancelInvoke("Attack");
+            isAttacking = false;
+        }
         switch (layerHit) {
             case enemyLayer:
                 var enemy = raycastHit.collider.gameObject;
@@ -62,6 +80,42 @@ public class Player : MonoBehaviour, IDamageable {
                 break;
         }
     }
+
+
+    private void Attack() {
+        GameObject newProjectile = Instantiate(projectilePrefab, projectileSocket.transform.position, Quaternion.identity);
+        var projectileComponent = newProjectile.GetComponent<Projectile>();
+        projectileComponent.setDamage(damagePerShot);
+        projectileComponent.setLifetime(projectileLifetime);
+        projectileComponent.setSpeed(projectileSpeed);
+
+
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, transform.position);
+        float distance;
+        if (plane.Raycast(ray, out distance)) {
+            Vector3 target = ray.GetPoint(distance);
+            Vector3 direction = target - projectileSocket.transform.position;
+            float rotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            newProjectile.GetComponent<Rigidbody>().velocity = direction * projectileComponent.getSpeed();
+
+        }
+
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(mousePos);
+        mousePos.z = 0;
+        Vector3 playerToMouse = (mousePos - projectileSocket.transform.position).normalized;
+        Debug.Log("ptm" + playerToMouse);
+
+
+
+
+        
+
+    }
+
 
 
 }
