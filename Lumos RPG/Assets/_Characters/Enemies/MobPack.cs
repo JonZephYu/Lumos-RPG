c21047607b7fba3de0854c3c9c4a6f48fa0453e7
Jon Yu <jon.yu.1105@gmail.com>
2019-06-07 18:49:18 -0400

    Enemies are turned inactive at the start, and then reactivated when their parent 'Mob Pack' is near the player.  (Had led to slight performance increase, still having some issues).

---
+++ Lumos RPG/Assets/_Characters/Enemies/MobPack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobPack : MonoBehaviour {
+
+    [SerializeField] float activateRadius = 10f;
+
+    private GameObject player = null;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Awake() {
+        // Loop through children and turn off
+        foreach (Transform child in transform) {
+            Debug.Log(child + " was turned off");
+            child.transform.gameObject.SetActive(false);
+        }
+
+    }
+
+    private void Update() {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (distanceToPlayer <= activateRadius) {
+            ActivateChildren();
+        }
+
+    }
+
+    private void ActivateChildren() {
+        foreach (Transform child in transform) {
+            Debug.Log(child + " was activated");
+            child.transform.gameObject.SetActive(true);
+        }
+
+
+        throw new NotImplementedException();
+    }
+
+    private void OnDrawGizmos() {
+        //Draw movement gizmos
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, activateRadius);
+
+    }
+
+}
